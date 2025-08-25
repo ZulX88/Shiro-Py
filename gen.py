d783503e345a8f683fff6a546355410bdc3f08ef
ZulX88 <zhanprasetyaningrat@gmail.com>
2025-08-25 07:52:08 +0700

    update

--- gen.py
+++ gen.py
@@ -1,6 +1,6 @@
 from neonize.client import NewClient
-
-client = NewClient("db.sqlite3")
+import config
+client = NewClient(config.namedb)
 
 number = input("Nomor WA : ")
 client.PairPhone(str(number), show_push_notification=True)
